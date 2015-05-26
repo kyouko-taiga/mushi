45360ddea39d6500027a8b95ea56ac886d5f416c
Dimitri Racordon <kyouko.taiga@gmail.com>
2015-05-26 23:12:13 +0200

    pretty print json list responses

--- mushi/core/utils/http.py
+++ mushi/core/utils/http.py
@@ -14,8 +14,22 @@
 
 import json
 
-from flask import Response, current_app
+from flask import current_app, request
 
 
 def jsonify_list(data):
-    return Response(json.dumps(data, cls=current_app.json_encoder), mimetype='application/json')
+    indent = None
+    separators = (',', ':')
+
+    if current_app.config['JSONIFY_PRETTYPRINT_REGULAR'] \
+       and not request.is_xhr:
+        indent = 2
+        separators = (', ', ': ')
+
+    # Note that we add '\n' to end of response
+    # (see https://github.com/mitsuhiko/flask/pull/1262)
+    rv = current_app.response_class(
+        (json.dumps(data, indent=indent, separators=separators, cls=current_app.json_encoder),
+         '\n'),
+        mimetype='application/json')
+    return rv
