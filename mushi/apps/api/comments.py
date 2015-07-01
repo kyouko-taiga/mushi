1cb37c8c50ce5dc36f6f737b6f5bf8f04d9f7ee0
Secaly <kevin.ledieu.fr@gmail.com>
2015-07-01 14:07:33 +0200

    add update_comment function

--- mushi/apps/api/comments.py
+++ mushi/apps/api/comments.py
@@ -19,7 +19,7 @@ from mushi.core.auth import require_auth_token
 from mushi.core.db import db_session
 from mushi.core.db.models import Issue, Comment
 from mushi.core.utils.http import jsonify_list
-
+from mushi.core.utils.time import utcnow
 
 bp = Blueprint('comments', __name__)
 
@@ -27,28 +27,24 @@ bp = Blueprint('comments', __name__)
 @bp.route('/comments/')
 @require_auth_token
 def list_comments(auth_token):
-    # try:
-    #     return render_template('comments')
-    # except NoResultFound:
-    #     abort(404)
 
     return jsonify(comment.to_dict(max_depth=2))
 
 
 @bp.route('/comments/<uid>')
 @require_auth_token
-def show_issue(auth_token, uid):
+def show_comment(auth_token, uid):
     try:
-        issue = db_session.query(Comment).filter(Comment.uid == uid).one()
+        comment = db_session.query(Comment).filter(Comment.uid == uid).one()
     except NoResultFound:
         abort(404)
 
-    return jsonify(issue.to_dict(max_depth=2))
+    return jsonify(comment.to_dict(max_depth=2))
 
 
 @bp.route('/comments/', methods=['POST'])
 @require_auth_token
-def create_issue(auth_token):
+def create_comment(auth_token):
     try:
         post_data = request.get_json(force=True)
     except BadRequest as e:
@@ -66,7 +62,7 @@ def create_issue(auth_token):
 
 @bp.route('/comments/<uid>', methods=['POST', 'PUT'])
 @require_auth_token
-def update_issue(auth_token, uid):
+def update_comment(auth_token, uid):
     try:
         comment = db_session.query(Comment).filter(Comment.uid == uid).one()
     except NoResultFound:
@@ -77,6 +73,10 @@ def update_issue(auth_token, uid):
     except BadRequest as e:
         raise ApiError(e.description)
 
+    post_data['updated_at'] = utcnow()
+
+    comment.update(post_data)
+
     db_session.commit()
 
     return jsonify(comment.to_dict(max_depth=2))
@@ -84,7 +84,7 @@ def update_issue(auth_token, uid):
 
 @bp.route('/comments/<uid>', methods=['DELETE'])
 @require_auth_token
-def delete_issue(auth_token, uid):
+def delete_comment(auth_token, uid):
     try:
         db_session.query(Comment).filter(Comment.uid == uid).delete()
     except NoResultFound:
